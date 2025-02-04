e0140d09abab14ab960c96bec2615010dc898aa1
hoangvm <hoangvm@stageit.app>
2025-02-04 16:44:16 +0700

    refactor table

--- src/components/custom-tanstack-table/body/tankstack-table-body.tsx
+++ src/components/custom-tanstack-table/body/tankstack-table-body.tsx
@@ -1,29 +1,29 @@
-import { Column, flexRender, Table } from '@tanstack/react-table';
+import { flexRender } from '@tanstack/react-table';
 import { useTableContext } from '../../../context/tanstack-table/table-context';
+import { getCommonPinningStyle } from '../../../utils';
 
 function TableBody<T>() {
   const { tableState } = useTableContext<T>();
-  const getCommonPinningStyle = <T,>(column: Column<T>) => {
-    const isPinned = column.getIsPinned(); //left - right - false
-    const isLastLeftPinnedColumn =
-      isPinned === 'left' && column.getIsLastColumn("left");
-    const isFirstRightPinnedColumn =
-      isPinned === 'right' && column.getIsFirstColumn("right");
+ 
+  //   const isLastLeftPinnedColumn =
+  //     isPinned === 'left' && column.getIsLastColumn("left");
+  //   const isFirstRightPinnedColumn =
+  //     isPinned === 'right' && column.getIsFirstColumn("right");
 
-    return {
-      boxShadow: isLastLeftPinnedColumn
-        ? '-4px 0 4px -4px gray inset'
-        : isFirstRightPinnedColumn
-          ? '4px 0 4px -4px gray inset'
-          : undefined,
-      left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
-      right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
-      opacity: isPinned ? 0.95 : 1,
-      position: isPinned ? ('sticky' as const) : ('relative' as const),
-      width: column.getSize(),
-      zIndex: isPinned ? 1 : 0,
-    };
-  };
+  //   return {
+  //     boxShadow: isLastLeftPinnedColumn
+  //       ? '-4px 0 4px -4px gray inset'
+  //       : isFirstRightPinnedColumn
+  //         ? '4px 0 4px -4px gray inset'
+  //         : undefined,
+  //     left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
+  //     right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
+  //     opacity: isPinned ? 0.95 : 1,
+  //     position: isPinned ? ('sticky' as const) : ('relative' as const),
+  //     width: column.getSize(),
+  //     zIndex: isPinned ? 1 : 0,
+  //   };
+  // };
   return (
     <tbody>
       {tableState.getRowModel().rows.map((row) => {
--- src/components/custom-tanstack-table/header/tankstack-table-header.tsx
+++ src/components/custom-tanstack-table/header/tankstack-table-header.tsx
@@ -1,31 +1,9 @@
 import { Column, flexRender, Table } from '@tanstack/react-table';
 import './tankstack-table-header.scss';
 import { useTableContext } from '../../../context/tanstack-table/table-context';
+import { getCommonPinningStyle } from '../../../utils';
 function TableHeader<T>() {
   const { tableState } = useTableContext<T>();
-
-  const getCommonPinningStyle = <T,>(column: Column<T>) => {
-    const isPinned = column.getIsPinned(); //left - right - false
-    const isLastLeftPinnedColumn =
-      isPinned === 'left' && column.getIsLastColumn('left');
-    const isFirstRightPinnedColumn =
-      isPinned === 'right' && column.getIsFirstColumn("right");
-
-    return {
-      boxShadow: isLastLeftPinnedColumn
-        ? '-4px 0 4px -4px gray inset'
-        : isFirstRightPinnedColumn
-          ? '4px 0 4px -4px gray inset'
-          : undefined,
-      left: isPinned === 'left' ? `${column.getStart('left')}px` : undefined,
-      right: isPinned === 'right' ? `${column.getAfter('right')}px` : undefined,
-      opacity: isPinned ? 0.95 : 1,
-      position: isPinned ? ('sticky' as const) : ('relative' as const),
-      width: column.getSize(),
-      zIndex: isPinned ? 1 : 0,
-      // background:isPinned ? "white" : ""
-    };
-  };
 
   return (
     <thead>
